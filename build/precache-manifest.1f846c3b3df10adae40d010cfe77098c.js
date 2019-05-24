6e6c445073a6c0f05e99d4aba72fda5f5f8fee6e
webmaster83 <nachit.n@gmail.com>
2019-05-24 16:55:43 +0200

    build2

---
+++ build/precache-manifest.1f846c3b3df10adae40d010cfe77098c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed27a8183e21a0c70da442fa7494a555",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f89250ede5a55b0996a",
+    "url": "/static/css/main.81d69fd7.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "9f89250ede5a55b0996a",
+    "url": "/static/js/main.698285a4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dcf756a898302d40ee3b163beab85ad4",
+    "url": "/static/media/fond1.dcf756a8.png"
+  }
+]);
